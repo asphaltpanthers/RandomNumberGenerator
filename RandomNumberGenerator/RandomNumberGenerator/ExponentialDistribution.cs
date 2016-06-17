08a94738e2aefd82dc25783c680328ee1a6140ad
diser <diser@asphaltpanthers.com>
2016-06-16 22:28:51 -0400

    Add a generator for exponential distribution

---
+++ RandomNumberGenerator/RandomNumberGenerator/ExponentialDistribution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomNumberGenerator
+{
+    public class ExponentialDistribution
+    {
+        private Random _random = new Random();
+
+        public Double Next(Double rate)
+        {
+            var uniformlyRandomNumber = _random.NextDouble();
+
+            return Math.Log(1 - uniformlyRandomNumber) / rate;
+        }
+    }
+}
